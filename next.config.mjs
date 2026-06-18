29a496c218bebd4d27e4941523111b3576549ca0
Alif <alifmuhamadaramdan75@gmail.com>
2026-06-18 19:22:33 +0700

    prototype

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Fix for Windows ESM URL scheme error
+    config.output.webassemblyModuleFilename = '../static/wasm/[modulehash].wasm';
+    
+    return config;
+  },
+};
 
 export default nextConfig;
